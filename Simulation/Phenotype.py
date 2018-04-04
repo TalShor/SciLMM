dc4041e7740470211f9a4fccf64d4f2c133fdce0
Ubuntu <ubuntu@ip-10-217-38-98.ec2.internal>
2018-04-04 07:44:55 +0000

    Addded examples

--- Simulation/Phenotype.py
+++ Simulation/Phenotype.py
@@ -19,3 +19,17 @@ def simulate_phenotype(covariance_matrices, covariate_matrix, sigmas, fixed_effe
 
     y += covariate_matrix.dot(fixed_effects)
     return (y - y.mean()) / y.std()
+
+
+def quick_simulate_phenotype(ibd_L, covariate_matrix, sigma_g, fixed_effects, add_intercept=False):
+    n = ibd_L.shape[0]
+    sim = [ibd_L.dot(np.random.randn(n)), np.random.randn(n)]
+    sim = np.array(sim).T
+    sim = (sim - sim.mean(axis=0)) / sim.std(axis=0)
+    y = sim.dot(np.sqrt(np.array([sigma_g, 1 - sigma_g])))
+
+    if add_intercept:
+        covariate_matrix = np.hstack((covariate_matrix, np.ones((n, 1))))
+
+    y += covariate_matrix.dot(fixed_effects)
+    return (y - y.mean()) / y.std()
